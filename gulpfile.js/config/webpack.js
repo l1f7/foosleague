f05247453d792fc30173b9e46b2be3b7bf1bf02a
Adrian Gyuricska <a.gyuricska@gmail.com>
2015-05-29 20:38:54 -0600

    WIP

--- gulpfile.js/config/webpack.js
+++ gulpfile.js/config/webpack.js
@@ -3,17 +3,15 @@ var config          = require('./')
 ,   webpackManifest = require('../lib/webpackManifest')
 ,   argv            = require('yargs').argv;
 
-module.exports = function (env) {
+module.exports = function(env) {
   var jsDest
   ,   jsSrc  = config.sourceDirectory + 'js/';
 
   jsDest = (argv.proto) ? config.prototypeAssets + 'js/' : config.appAssets + 'js/'
 
   var webpackConfig = {
-    // Entry will be the files created by Webpack,
-    // the key is the name of the js file and it's
-    // value is the "entry point" (source file) for
-    // the javascript input
+    // Entry will be the files created by Webpack
+    // (in this case there would be two files)
     src : jsSrc + '**',
     entry: {
       app:  [jsSrc + 'App.js']
@@ -21,37 +19,16 @@ module.exports = function (env) {
 
     output: {
       path: jsDest,
-      filename: '[name].js',
-      publicPath: (argv.proto) ? config.prototypeAssets + 'js/' : '/static/js/'
+      //filename: '[name].js',
+      // from greypants' gulp starter, not sure the use yet
+      filename: env === 'production' ? '[name]-[hash].js' : '[name].js',
+      publicPath: '/assets/js/'
     },
 
-    if (argv.proto) {
-      plugins: [
-        new webpack.ProvidePlugin({
-          $: "jquery",
-          jQuery: "jquery",
-          "window.jQuery": "jquery",
-          "GLOBAL": "./global"
-        })
-      ],
-    } else {
-      plugins: [
-        new webpack.ProvidePlugin({
-          $: "jquery",
-          jQuery: "jquery",
-          "window.jQuery": "jquery",
-          "GLOBAL": "./global"
-        }),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin(),
-        new webpack.NoErrorsPlugin()
-      ],
-    }
+    plugins: [],
 
     resolve: {
-      extensions: ['', '.js', '.jsx', '.coffee'],
-      modulesDirectories: ['node_modules', 'raw/bower', 'raw/js'],
-      root: __dirname
+      extensions: ['', '.js', '.jsx', '.coffee']
     },
 
     module: {
@@ -60,14 +37,38 @@ module.exports = function (env) {
           test: /\.js$/,
           loader: 'babel-loader?stage=1',
           exclude: /node_modules/
-        },
-        {
-          test: /\.coffee$/,
-          loader: 'coffee-loader',
-          exclude: /node_modules/
         }
       ]
     }
+  }
+
+  // if (env !== 'test') {
+  //   // Factor out common dependencies into a common.js
+  //   webpackConfig.plugins.push(
+  //     new webpack.optimize.CommonsChunkPlugin({
+  //       name: 'common',
+  //       filename: env === 'production' ? '[name]-[hash].js' : '[name].js',
+  //     })
+  //   )
+  // }
+
+  if (env === 'development' || env === 'prototype') {
+    webpackConfig.devtool = 'source-map'
+    webpack.debug = true
+  }
+
+  if (env === 'production') {
+    webpackConfig.plugins.push(
+      new webpackManifest(publicPath, 'public'),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.NoErrorsPlugin()
+    )
   }
 
   return webpackConfig
---
+++ prototype/assets/js/1.1.js
@@ -0,0 +1,16 @@
+webpackJsonp([1],{
+
+/***/ 2:
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	module.exports = {
+	  init: function init() {
+	    console.log('feed view initialized');
+	  }
+	};
+
+/***/ }
+
+});
