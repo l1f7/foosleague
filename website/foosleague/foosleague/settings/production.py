b46dbc9351617917624d2f5c197a018512f73078
Scott Barkman <scott@liftinteractive.com>
2015-06-01 11:11:42 -0600

    update production

--- website/foosleague/foosleague/settings/production.py
+++ website/foosleague/foosleague/settings/production.py
@@ -57,6 +57,7 @@ CACHES = {}
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = get_env_setting('SECRET_KEY')
 ########## END SECRET CONFIGURATION
+SECRET_KEY = r"t4%w_uy@-hr!_e@pm9h4694&amp;t2*=r^mp4vw#dsvfh_+41$yfx+"
 
 
 ########## RAVEN
