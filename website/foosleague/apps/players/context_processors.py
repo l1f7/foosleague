a61518b9167eb30064eccc1995a9d096e2131701
Scott Barkman <scott@liftinteractive.com>
2015-06-02 08:46:42 -0600

    no player fall back

--- website/foosleague/apps/players/context_processors.py
+++ website/foosleague/apps/players/context_processors.py
@@ -2,5 +2,10 @@ from .models import Player
 
 
 def player(request):
-    player = Player.objects.get(user=request.user)
-    return {'player': player}
+    if request.user.is_authenticated():
+        try:
+            player = Player.objects.get(user=request.user)
+            return {'player': player}
+        except:
+            return {'player': ''}
+
