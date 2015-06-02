5a16106c120139ed14207a57a58db2771f76ed5a
Scott Barkman <scott@liftinteractive.com>
2015-06-02 09:03:05 -0600

    update context

--- website/foosleague/apps/players/context_processors.py
+++ website/foosleague/apps/players/context_processors.py
@@ -2,10 +2,10 @@ from .models import Player
 
 
 def player(request):
-    if request.user.is_authenticated():
-        try:
-            player = Player.objects.get(user=request.user)
-            return {'player': player}
-        except:
-            return {'player': ''}
+    try:
+        player = Player.objects.get(user=request.user)
+        return {'player': player}
+    except:
+
+        return {}
 
