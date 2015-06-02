f1fb139416e22827ff635b5db4b89d63b09d918f
Scott Barkman <scott@liftinteractive.com>
2015-06-02 09:25:35 -0600

    update foosleague urls

--- website/foosleague/foosleague/urls.py
+++ website/foosleague/foosleague/urls.py
@@ -18,7 +18,7 @@ urlpatterns = patterns('',
     url(r'^matches/', include('matches.urls')),
     url(r'^bookie/', include('bookie.urls')),
     url(r'^seasons/', include('seasons.urls')),
-    url(r'^tease/', TemplateView.as_view(template_name='tease.html')),
+    url(r'^/', TemplateView.as_view(template_name='tease.html')),
     url(r'^robots\.txt/$', TemplateView.as_view(template_name='robots.txt',
         content_type='text/plain')),
 
