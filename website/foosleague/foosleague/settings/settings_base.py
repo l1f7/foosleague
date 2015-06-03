4008bf18a939e39d9b655a73f0cd921353d9e31b
Scott Barkman <scott@liftinteractive.com>
2015-06-03 12:32:40 -0600

    fix slow db connection

--- website/foosleague/foosleague/settings/settings_base.py
+++ website/foosleague/foosleague/settings/settings_base.py
@@ -147,7 +147,7 @@ DATABASES = {
         'NAME': 'foosleague',    # Or path to database file if using sqlite3.
         'USER': 'foosleague',             # Not used with sqlite3.
         'PASSWORD': 'foosleague',     # Not used with sqlite3.
-        'HOST': '198.199.96.37',   # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '127.0.0.1',   # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                 # Set to empty string for default. Not used with sqlite3.
     }
 }
