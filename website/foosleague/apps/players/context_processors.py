a5ca4aa378791bb45ed23468e8ca7109e2f3d3e0
Scott Barkman <scott@liftinteractive.com>
2015-06-12 23:00:03 -0600

    update some context processors yo

--- website/foosleague/apps/players/context_processors.py
+++ website/foosleague/apps/players/context_processors.py
@@ -1,12 +1,93 @@
 from .models import Player
+from datetime import datetime, timedelta
+from django.db.models import Q, Sum
+
+from teams.models import Team
+from matches.models import Match
+from seasons.models import Season
 
 
 def player(request):
-    try:
-        player = Player.objects.get(user=request.user)
+    # try:
+    today = datetime.today()
 
-        return {'player': player}
-    except:
+    player = Player.objects.get(user=request.user)
+    teams = Team.objects.filter(players=player)
+    current_season = Season.objects.filter(league=request.league, start__lte=today, end__gte=today)
 
-        return {}
+    matches = Match.objects.filter(Q(team_1__in=teams) | Q(team_2__in=teams), league=request.league)
+    wins = {
+        '7': matches.filter(winner__in=teams, created__gte=today - timedelta(days=7), created__lte=today).count(),
 
+        '30': matches.filter(winner__in=teams, created__gte=today - timedelta(days=30), created__lte=today).count(),
+        'season': matches.filter(winner__in=teams, season=current_season).count()
+    }
+    losses = {
+        '7': matches.filter(created__gte=today - timedelta(days=7), created__lte=today).exclude(winner__in=teams).count(),
+
+        '30': matches.filter(created__gte=today - timedelta(days=30), created__lte=today).exclude(winner__in=teams).count(),
+        'season': matches.filter(winner__in=teams, season=current_season).exclude(winner__in=teams).count()
+    }
+
+    # 7 day goal differential
+    goals_for = {}
+    goals_against = {}
+
+    # team 1 goals
+    team1_goals = matches.filter(created__gte=today - timedelta(days=7), created__lte=today,
+                                 team_1__in=teams).aggregate(goals_as_team1=Sum('team_1_score'), goals_against=Sum('team_2_score'))
+    goals_for[7] = team1_goals['goals_as_team1']
+    goals_against[7] = team1_goals['goals_against']
+
+    # team 2 goals
+    team2_goals = matches.filter(created__gte=today - timedelta(days=7), created__lte=today,
+                                 team_2__in=teams).aggregate(goals_as_team2=Sum('team_2_score'), goals_against=Sum('team_1_score'))
+    goals_for[7] += team2_goals['goals_as_team2'] or 0
+    goals_against[7] += team2_goals['goals_against'] or 0
+
+    # 30 day goal differential
+    team1_goals = matches.filter(created__gte=today - timedelta(days=7), created__lte=today,
+                                 team_1__in=teams).aggregate(goals_as_team1=Sum('team_1_score'), goals_against=Sum('team_2_score'))
+    goals_for[30] = team1_goals['goals_as_team1'] or 0
+    goals_against[30] = team1_goals['goals_against'] or 0
+
+    # team 2 goals
+    team2_goals = matches.filter(created__gte=today - timedelta(days=30), created__lte=today,
+                                 team_2__in=teams).aggregate(goals_as_team2=Sum('team_2_score'), goals_against=Sum('team_1_score'))
+    goals_for[30] += team2_goals['goals_as_team2'] or 0
+    goals_against[30] += team2_goals['goals_against'] or 0
+
+    # Season goal differential
+    team1_goals = matches.filter(team_1__in=teams, season=current_season).aggregate(
+        goals_as_team1=Sum('team_1_score'), goals_against=Sum('team_2_score'))
+    goals_for['season'] = team1_goals['goals_as_team1'] or 0
+    goals_against['season'] = team1_goals['goals_against'] or 0
+
+    # team 2 goals
+    team2_goals = matches.filter(team_2__in=teams, season=current_season).aggregate(
+        goals_as_team2=Sum('team_2_score'), goals_against=Sum('team_1_score'))
+    goals_for['season'] += team2_goals['goals_as_team2'] or 0
+    goals_against['season'] += team2_goals['goals_against'] or 0
+
+    goal_differential = {
+        '7': {
+            'for': goals_for[7],
+            'against': goals_against[7]
+        },
+        '30': {
+            'for': goals_for[30],
+            'against': goals_against[30]
+        },
+        'season': {
+            'for': goals_for['season'],
+            'against': goals_against['season']
+        }
+    }
+
+    stats = {
+        'wins': wins,
+        'losses': losses,
+        'goal_differential': goal_differential
+    }
+
+    return {'player': player, 'stats': stats}
