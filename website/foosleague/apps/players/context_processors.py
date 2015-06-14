34eb8aa76a13012d53c46994b129e8bdf381a228
Scott Barkman <scott@liftinteractive.com>
2015-06-13 22:10:12 -0600

    derp

--- website/foosleague/apps/players/context_processors.py
+++ website/foosleague/apps/players/context_processors.py
@@ -92,11 +92,11 @@ def player(request):
 
         color_stats = {
             'red': {
-                'favoured': int(round((red_wins/red_plays)*100))
+                'favoured': int(round((red_wins/red_plays)*100)),
                 'winning_percentage': int(round((red_plays/(red_plays+black_plays))*100))
             },
             'black': {
-                'favoured': int(round((black_wins/black_plays)*100))
+                'favoured': int(round((black_wins/black_plays)*100)),
                 'winning_percentage': int(round((black_plays/(red_plays+black_plays))*100))
             }
         }
