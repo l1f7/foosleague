4fafcf4b22ac98b0ff3f1eaa23b26e1248dd0e13
Scott Barkman <scott@liftinteractive.com>
2015-06-13 21:45:02 -0600

    broke subdomain routing

--- website/foosleague/apps/leagues/middleware.py
+++ website/foosleague/apps/leagues/middleware.py
@@ -2,28 +2,21 @@ from django.shortcuts import get_object_or_404
 from django.http.response import HttpResponseRedirect
 from django.conf import settings
 import re
-from leagues.models import League, LeagueMember
-from players.models import Player
+from leagues.models import League
 subdomain_pattern = re.compile('(?P<subdomain>.*?)\..*?')
-
 
 class SubdomainMiddleware(object):
 
+
     def process_request(self, request):
-        match = subdomain_pattern.match(request.get_host())
-        subdomain = match.group('subdomain')
-
-        if subdomain != 'foosleague' and subdomain != 'www':
-            league = get_object_or_404(League.objects.filter(subdomain=subdomain))
-            try:
-                lm = LeagueMember.objects.get(league=league, player__user=request.user)
-            except:
-                return HttpResponseRedirect('http://foosleague.com/tease/')
-
-
-        else:
-            # for now
-            return HttpResponseRedirect('http://liftinteractive.foosleague.com/matches/')
+        if settings.DEBUG:
+            match = subdomain_pattern.match(request.get_host())
+            subdomain = match.group('subdomain')
+            if subdomain != 'foosleague' and subdomain != 'www':
+                league = get_object_or_404(League.objects.filter(subdomain=subdomain))
+            else:
+                # for now
+                return HttpResponseRedirect('http://liftinteractive.foosleague.com/matches/')
 
         if request.user:
             #make sure user is apart of league
