0d4c17507838d884ff660f8304ed50d89f38b2ba
Scott Barkman <scott@liftinteractive.com>
2015-06-14 00:00:21 -0600

    more streakage

--- website/foosleague/apps/players/utils.py
+++ website/foosleague/apps/players/utils.py
@@ -3,9 +3,8 @@ from teams.models import Team
 from django.db.models import Q
 
 def get_streaks(player):
-    matches = player.matches
+    matches = player.matches.order_by('created')
 
-    print matches
     win_streak = 0
     best_win_streak = 0
     losing_streak= 0
@@ -29,15 +28,23 @@ def get_streaks(player):
                     # first loss, mark the date
                     worst_losing_streak_date = m.created
 
+    not_lost_since = None
+    not_won_since = None
+
+    if win_streak:
+        not_lost_since = matches.exclude(winner=player.teams)[0].created
+    if losing_streak:
+        not_won_since = matches.filter(winner=player.teams)[0].created
+
     streaks = {
         'win_streak': win_streak,
         'best_win_streak': best_win_streak,
         'best_win_streak_date': best_win_streak_date,
-        'not_lost_since': matches.exclude(winner=player.teams)[0].created,
+        'not_lost_since': not_lost_since,
         'losing_streak': losing_streak,
         'worst_losing_streak': worst_losing_streak,
         'worst_losing_streak_date': worst_losing_streak_date,
-        'not_won_since': matches.filter(winner=player.teams)[0].created,
+        'not_won_since': not_won_since,
     }
     return streaks
 
