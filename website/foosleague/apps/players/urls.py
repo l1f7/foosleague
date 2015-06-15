f4f8d5499ab5cec701acf9a261452fa9ede8303d
Scott Barkman <scott@liftinteractive.com>
2015-06-15 08:33:14 -0600

    update players

--- website/foosleague/apps/players/urls.py
+++ website/foosleague/apps/players/urls.py
@@ -4,6 +4,6 @@ from .views import PlayerListView, PlayerUpdateView, PlayerDetailView
 
 urlpatterns = patterns('',
                        url(r'^$', PlayerListView.as_view(), name='players'),
-                       url(r'^settings/$', PlayerUpdateView.as_view(), name='players-update'),
+                       url(r'^(?P<pk>[0-9]+)/settings/$', PlayerUpdateView.as_view(), name='players-update'),
                        url(r'^(?P<pk>[0-9]+)/$', PlayerDetailView.as_view(), name='player-detail')
                        )
